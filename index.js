246e69b5dcaab8225c992168eebcb5b0350064ac
Stefania Bezerra da Silva <stefaniabezerra95@gmail.com>
2017-06-23 13:10:35 -0300

    Discipline index

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const mainPageReader = require('./Reader/mainPageReader');
+const html = require('./Grabber/htmlGrabber');
+const disciplineReader = require('./Reader/disciplineReader');
 
 mainPageReader.getDisciplines();
+
+disciplineReader.getDiscipline();
