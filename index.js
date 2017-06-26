79b084e686a4cb55f1efc22e5b532da663e4fbaf
Matheus Oliveira <matheus_oliveira30@hotmail.com>
2017-06-26 09:13:48 -0300

    Added demostration that was showed in class

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const mainPageReader = require('./Reader/mainPageReader');
 const disciplineReader = require('./Reader/disciplineReader');
+const regexDemo = require('./Resources/regexDemo');
+
 const specificUrl = 'https://matriculaweb.unb.br/graduacao/oferta_dados.aspx?cod=203882&dep=650';
+const gitUrl = 'https://github.com/GabrielDViana/IsCoolParser/wiki';
 
 // Gets all the first page data -> FGA MW
 exports.getAllDisciplines = () => {
@@ -15,3 +18,5 @@ exports.getSpecific = (url) => {
 // Uses the functions
 exports.getAllDisciplines();
 exports.getSpecific(specificUrl);
+
+regexDemo.getGitDemo(gitUrl);
